574ec4e4c197dd7c72824c68917ec6c041b2c9dd
CULT PONY <67918945+cultpony@users.noreply.github.com>
2023-10-12 13:28:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,5 +8,4 @@ sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-# Install Nix for convenience
-sh <(curl -L https://nixos.org/nix/install) --no-daemon
+source "$HOME/.cargo/env"
