9654193d694e2ec10e6f24d1f76a917b48521566
CULT PONY <67918945+cultpony@users.noreply.github.com>
2023-10-12 13:24:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y zsh
 sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 
 # Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- -y
 
 # Install Nix for convenience
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
